c4974465fca88fe9a66af0409b8d47f0e5ea6320
Vatsal0518 <134831076+Vatsal0518@users.noreply.github.com>
2024-07-27 12:13:36 +0530

    Add files via upload

--- src/connection_mongo/connections.js
+++ src/connection_mongo/connections.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://vatsal0909:vatsal0909@nfc.lje8smx.mongodb.net/?retryWrites=true&w=majority&appName=NFC").then(()=>{console.log("databse connected to NFC")}).catch((e)=>{console.log("error in connections wtih database "+ e)});
+mongoose.connect("mongodb+srv://vatsal0909:vatsal0909@nfc.sebbf5a.mongodb.net/?retryWrites=true&w=majority&appName=NFC").then(()=>{console.log("databse connected to NFC")}).catch((e)=>{console.log("error in connections wtih database "+ e)});
